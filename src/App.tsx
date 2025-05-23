2fcb35166dcda589ca9aedb3fcbab018af5d6106
Alessandra Petry <alessandrapetryy@gmail.com>
2025-05-23 11:35:11 +0200

    feat: fetch and render items in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
-  return;
+  const [items, setItems] = useState<string[]>([]);
+
+  useEffect(() => {
+    fetch("/items.json")
+      .then((res) => res.json())
+      .then((json) => setItems(json.data))
+      .catch((err) => console.error(err));
+  }, []);
+
+  console.log("items", items);
+
+  return <>{items && items.map((item) => <p>{item}</p>)}</>;
 }
 
 export default App;
