f7f58529a8f7dc0a04ef3e75d924761e509c1346
Alessandra Petry <alessandrapetryy@gmail.com>
2025-05-24 16:15:02 +0200

    refactor: remove unused state and effect from App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,6 @@
-import { useEffect, useState } from "react";
-import { FilterBox } from "./components/FilterBox";
+import { FilterBox } from './components/FilterBox';
 
 function App() {
-  const [items, setItems] = useState<string[]>([]);
-
-  useEffect(() => {
-    fetch("/items.json")
-      .then((res) => res.json())
-      .then((json) => setItems(json.data))
-      .catch((err) => console.error(err));
-  }, []);
-
-  console.log("items", items);
-
   return <FilterBox />;
 }
 
